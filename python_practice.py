e1900cb0e63a5d1ebc2f0733f7164a0bdb51526a
Deepikaranigadge <deepam.sabhuo1@gmail.com>
2025-11-26 23:02:55 +0530

    python_practice

---
+++ python_practice.py
@@ -0,0 +1,102 @@
+print("Good Day")
+#output: Good Day
+
+x= 10
+y= 20
+print(x+y)
+#output: 30
+
+pi =3.142
+r=2
+print(2*pi*r)
+#output:12.568
+
+x, y = 10, 20
+print(y)
+print(x)
+#output:20
+10
+
+x =10,20
+print(x)
+#output:(10, 20)
+
+x= 10,20,30
+print(x)
+#output:(10, 20, 30)
+
+x=10
+x="India"
+print(x)
+#output:India
+
+x=10
+x="India"
+x=[20, 40]
+print(x)
+#output:[20, 40]
+
+x="Beginner"
+print(x)
+x="good"
+x="expert"
+#output:Beginner
+
+x="Beginner"
+print(x)
+x="Good"
+print(x)
+x="Expert"
+print(x)
+#output:Beginner Good Expert
+
+person1 = person2 = person3 = "Expert"
+print(person1)
+#output:Expert Expert Expert
+
+person1 = person2 = person3 = "Expert"
+print(person1)
+print(person2)
+print(person3)
+#output:('Expert', 'Good')
+#('Expert', 'Good')
+#('Expert', 'Good')
+
+person1 = person2 = person3 = "Expert" , "Good"
+print(person1)
+print(person2)
+print(person3)
+#output:
+
+person1, person2, person3 = "Expert" , "Good", "Good"
+print(person1)
+print(person2)
+print(person3)
+#output:Expert
+#Good
+#Good
+
+#person1, person2, person3 = "Expert" , "Good"
+#print(person1)
+#print(person2)
+#print(person3)
+#output: ERROE
+
+print("\n\n")
+#output: NEW LINE
+
+x=20
+print("value of x is ", x)
+#output:value of x is  20
+
+x=            20
+print("value of x is ", x)
+#output:value of x is  20
+
+
+
+
+
+
+
+

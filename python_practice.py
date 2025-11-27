f1ab7420485ceb712ce32a515c5445199350493a
Deepikaranigadge <deepam.sabhuo1@gmail.com>
2025-11-27 22:26:46 +0530

    String_practies

--- python_practice.py
+++ python_practice.py
@@ -94,9 +94,84 @@ print("value of x is ", x)
 #output:value of x is  20
 
 
+#              Date 27 /11 /25
+number1 = 10
+print(type(number1))
+#output :<class 'int'>
 
+number1 = 10.4
+print(type(number1))
+#output : <class 'float'>
 
+number1 = 10.4 + 20j
+print(type(number1))
+#output: <class 'complex'>
 
+number1 = 10.4 + 20
+print(type(number1))
+#output: <class 'float'>
 
+number1 = 10 + 20.4
+print(type(number1))
+#output: <class 'float'>
+
+number1 = 10.4
+print(type(number1))
+#output: <class 'float'>
+
+str1 = ''
+print(type(str1))
+# output: <class 'str'>
+
+str1 =""
+print(type(str1))
+# output: <class 'str'>
+
+str1 = ''' '''
+print(type(str1))
+# output: <class 'str'>
+
+str1 = ''''''
+print(type(str1))
+# output: <class 'str'>
+
+str1 = str()
+print(type(str1))
+# output: <class 'str'>
+
+str1 = str(123)
+print(type(str1))
+# output: <class 'str'>
+
+first_name = 'virat'
+print(len(first_name))
+#output : 5
+
+first_name = 'virat kolhi'
+print(len(first_name))
+#output : 11
+
+first_name = ''
+print(len(first_name))
+#output : 0
+
+first_name = ' '
+print(len(first_name))
+#output : 1
+
+first_name = 'VIRAT'
+print(len(first_name))
+print("The value at position 0", first_name[0])
+print("The value at position 4", first_name[4])
+#output :The value at position 0 V
+#        The value at position 4 T
+
+#first_name = 'VIRAT'
+#print("The value at position 5", first_name[5])
+# error string index out of range
+
+first_name = 'VIRAT'
+print(first_name[5-1])
+#output : T
 
 

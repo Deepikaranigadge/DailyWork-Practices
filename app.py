ee5efd67dd3fcfebc0c813439d0826a6c3a4e505
Deepikaranigadge <deepam.sabhuo1@gmail.com>
2025-12-13 20:02:46 +0530

    Added Flask Project

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("home.html", user="Sachin")
+
+@app.route('/contact')
+def message_me():
+    return render_template('contact.html', email='supprot@itdefined.org', phone='+91 7676797274')
+
+@app.route('/about')
+def about_me():
+    details ={
+        'email':'supprot@itdefined.org',
+        'phone':'+91 7676797274',
+        'courses':['MERN', 'Django','Develops','Embedded']
+    }
+    return render_template('about.html', details= details)
+if __name__ == "__main__":
+    app.run(debug=True)
